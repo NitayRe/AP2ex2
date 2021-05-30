dfeb91d3af4bd38b2f71d879cf0a93886f927900
yoav amar <yoav.6hutc@gmail.com>
2021-05-30 22:14:44 +0300

    added notes

--- static/js_files/model_select.js
+++ static/js_files/model_select.js
@@ -1,9 +1,16 @@
+/**
+ * this class is in charge of the models list.
+ */
 class ModelSelect{
-
+    /**
+     *
+     * @param modelSelect the id of the model select
+     */
     constructor(modelSelect) {
         this.__modelSelect = document.getElementById(modelSelect);
     }
     /**
+     * add a new model to the list
      * @param model is a json with this structure:
      * { model_id: <int>, upload_time: <datetime>, status: “ready” | “pending” }
      */
@@ -25,7 +32,7 @@ class ModelSelect{
         mSpan.innerText = "Model Id: " + model.model_id +
             "\nUpload Time: " + model.upload_time +
             "\nStatus: " + model.status;
-
+        //add new listenet to the mspan
         mSpan.addEventListener('click', function () {
             if (!this.classList.contains('selected')) {
                 let prevSelected = this.parentNode.querySelector('.custom-option.selected');
@@ -45,6 +52,11 @@ class ModelSelect{
         this.__modelSelect.querySelector('.custom-options').appendChild(mSpan);
     }
 
+    /**
+     * retuns a list of all the panding models id's.
+     * @returns {*[]}  the list of the pending modelst IDs.
+     * @private
+     */
     __getPendingModelsIds(){
         let modelsIds = [];
         let pendings = this.__modelSelect.querySelector('.custom-options').querySelectorAll(`[data-status="pending"]`);
@@ -54,16 +66,21 @@ class ModelSelect{
         return modelsIds;
     }
 
+    /**
+     * update the models by the models list.
+     * @param models
+     */
     updateModels(models) {
         let penndingsIds = this.__getPendingModelsIds();
+        // we add wech new model to the modelselect. it a model is pending we ceck if it change the status
         models.forEach(function (model){
             let option = this.__modelSelect.querySelector('.custom-options').querySelector(`[data-value="${model.model_id}"]`);
-
+            // if the model wasn't in the modelselect before.
             if(option == null) {
                 this.addNewModel(model);
                 return;
             }
-
+            // if the model is already exist and is ready.
             if(!penndingsIds.includes(model.model_id)){
                 return;
             }
@@ -81,8 +98,13 @@ class ModelSelect{
         }.bind(this));
     }
 
+    /**
+     * delete the selected model
+     */
     deleteSelectedModel(){
+        // get the id of the selected model.
         let deleteId = parseInt(this.getSelectedModel());
+        // if it was alreay deleted.
         if(deleteId == -1) {
             return;
         }
@@ -95,13 +117,22 @@ class ModelSelect{
         deletedModel.parentNode.removeChild(deletedModel);
     }
 
+    /**#
+     * return the id of the selected model.
+     * @returns {string} the selected model id.
+     */
     getSelectedModel(){
         return this.__modelSelect.querySelector('.custom-select__trigger span').getAttribute("data-value");
     }
+
+    /**
+     * check if the selected model is ready.
+     * @returns {boolean}
+     */
     isSelectedModelReady(){
         let status = this.__modelSelect.querySelector('.custom-select__trigger span').getAttribute("data-status");
         return (status.localeCompare("ready") == 0);
     }
 }
-
+// create the model select.
 let modelSelect = new ModelSelect("model-select");
