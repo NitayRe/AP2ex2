e16875393633d27c8f7b152304388a1c93b4328e
yoav amar <yoav.6hutc@gmail.com>
2021-05-30 21:34:18 +0300

    add notes

--- static/js_files/anomaly_graph.js
+++ static/js_files/anomaly_graph.js
@@ -1,5 +1,12 @@
+/**
+ * this class is in charge of the graph.
+ * it can create new graph and update the points on the graph.
+ */
 class AnomalyGraph{
-
+    /**
+     * create a new graph.
+     * @param name the name of the graph
+     */
     constructor(name) {
         this.__data = -1;
         this.__anomaly = -1;
@@ -7,6 +14,7 @@ class AnomalyGraph{
         this.__chart = new Chart(name, {
           type: "scatter",
           data: {
+              //at first the graph is empty.
             datasets: []
           },
           options: {
@@ -36,23 +44,17 @@ class AnomalyGraph{
                         pan: {
                             // Boolean to enable panning
                             enabled: true,
-
                             mode: 'xy',
-
                             // On category scale, factor of pan velocity
                             speed: 20
                         },
-
                         // Container for zoom options
                         zoom: {
                             // Boolean to enable zooming
                             enabled: true,
-
                             // Enable drag-to-zoom behavior
                             wheel: true,
-
                             mode: 'xy',
-
                             // Speed of zoom via mouse wheel
                             // (percentage of zoom on a wheel event)
                             speed: 0.1
@@ -63,18 +65,30 @@ class AnomalyGraph{
         });
     }
 
+    /**
+     * this func display al the graphs of the vars from the varnames list
+     * @param varNames
+     */
     displayGraph(varNames) {
         if(this.__data == -1) {
             return;
         }
-
+        //update the datasets of the graph.
         this.__chart.data.datasets = this.__getDatasets(varNames);
-
+        //update the chart in order to show the new dataset.
         this.__chart.update();
     }
 
+    /**
+     * this func returs the dataset for the spacific vars in the varNames list.
+     *
+     * @param varNames - a list of vars to show.
+     * @returns {*[]} - a dataset of the choosen vars. This means the data of the points that those vars gets.
+     * @private
+     */
     __getDatasets(varNames){
         let datasets = [];
+        // for every name we add a new set to the datasets.
         varNames.forEach(function (name) {
             let xyValues = this.__getXYValues(name);
 
@@ -91,6 +105,13 @@ class AnomalyGraph{
 
         return datasets;
     }
+
+    /**
+     * this func returns the values of a certain var.
+     * @param name the var we return its data.
+     * @returns {*[]} list of dictionaries that map index to value.
+     * @private
+     */
     __getXYValues(name) {
         let xyValues = [];
 
@@ -103,20 +124,36 @@ class AnomalyGraph{
         return xyValues;
     }
 
+    /**
+     * set the graph data.
+     * @param data the new data
+     */
     setData(data) {
         this.__data = data;
     }
 
+    /**
+     * set the graph anomay
+     * @param anomaly the new anomaly
+     */
     setAnomaly(anomaly) {
         this.__anomaly = anomaly;
     }
 
+    /**
+     * this func returns the color of the point. if the point in anomaly the color is red,
+     * otherwise black,
+     * @param context the x value of the point.
+     * @returns {string} the color of the point
+     * @private
+     */
     __customColor(context) {
+        // the defult color is black.
         let color = "rgba(0,0,0,1)";
         if(this.__anomaly == -1) {
             return color;
         }
-
+        //check if the context is in any of the anomay span.
         let isFound = this.__anomaly[context.dataset.label].find(function (span) {
             return (span[0] <= context.dataIndex) && (context.dataIndex <= span[1]);
         });
@@ -126,6 +163,9 @@ class AnomalyGraph{
         return color;
     }
 
+    /**
+     * reset the chart zoom.
+     */
     resetZoom(){
         this.__chart.resetZoom();
     }
